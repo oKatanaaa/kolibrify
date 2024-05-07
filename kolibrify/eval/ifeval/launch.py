419afc8f30640368b48e6ec638b6b650c3bab12e
oKatanaaa <whitemarsstudios@gmail.com>
2024-05-07 16:47:03 +0000

    new: ifeval benchmark

---
+++ kolibrify/eval/ifeval/launch.py
@@ -0,0 +1,105 @@
+import subprocess
+import json
+import typer
+import os
+from typing_extensions import Annotated
+
+from kolibrify.predict import predict
+from kolibrify.config import load_training_config
+
+
+package_path = os.path.dirname(__file__)
+evaldata_path = os.path.join(
+    package_path, 
+    'data'
+)
+RU_EVAL_PATH = os.path.join(evaldata_path, 'ru.jsonl')
+EN_EVAL_PATH = os.path.join(evaldata_path, 'en.jsonl')
+
+
+def load_eval_data(path):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+    
+    lines = list(map(json.loads, lines))
+    
+    conversations = []
+    for line in lines:
+        conversations.append(
+            {'messages': [
+                {'role': 'user', 'content': line['prompt']}
+            ]}
+        )
+    return conversations
+
+
+def save_responses(conversations, path):
+    responses = []
+    for conv in conversations:
+        msgs = conv['messages']
+        responses.append(
+            {
+                'prompt': msgs[0]['content'],
+                'response': msgs[1]['content']
+            }
+        )
+    
+    with open(path, 'w', encoding='utf-8') as f:
+        for resp in responses:
+            f.write(json.dumps(resp, ensure_ascii=False) + '\n')
+
+def main(
+    config_path: Annotated[str, typer.Argument()],
+    eval_lang: Annotated[str, typer.Option()] = 'en',
+    backend: Annotated[str, typer.Option()] = 'vllm',
+    type: Annotated[str, typer.Option()] = 'last',
+    temp: Annotated[float, typer.Option()] = 0,
+    top_p: Annotated[float, typer.Option()] = 0.95,
+    max_output_tokens: Annotated[int, typer.Option()] = 4096
+):
+    _, config = load_training_config(config_path)
+    assert os.path.exists(os.path.join(config.output_dir, "merged")), "The model must be merged but is not."
+    assert eval_lang in ['en', 'ru'], f"Only 'ru' and 'en' are supported, but got {eval_lang}."
+    if eval_lang == 'en':
+        eval_file_path = EN_EVAL_PATH
+    else:
+        eval_file_path = RU_EVAL_PATH
+        
+    conversations = load_eval_data(eval_file_path)
+    conversations = predict(
+        config=config,
+        conversations=conversations,
+        backend=backend,
+        type=type,
+        temp=temp,
+        top_p=top_p,
+        max_output_tokens=max_output_tokens
+    )
+    
+    output_save_path = os.path.join(config.output_dir, eval_lang + '_ifeval_results')
+    os.makedirs(output_save_path, exist_ok=True)
+    responses_path = os.path.join(output_save_path, 'responses.jsonl')
+    save_responses(conversations, responses_path)
+    print('Saved responses to', responses_path)
+    
+    # Launch evaluation
+    logs_path = os.path.join(output_save_path, 'eval.logs')
+    if eval_lang == 'en':
+        arg_prefix = ["python",
+            "-m",
+            "kolibrify.eval.ifeval.en.evaluation_main"]
+    else:
+        arg_prefix = ["python",
+            "-m",
+            "kolibrify.eval.ifeval.ru.evaluation_main"]
+    with open(logs_path, 'w') as f:
+        subprocess.run(
+            arg_prefix + [
+            f"--input_data={eval_file_path}",
+            f"--input_response_data={responses_path}",
+            f"--output_dir={output_save_path}"],
+            stdout=f
+        )
+
+def run():
+    typer.run(main)
--- kolibrify/predict.py
+++ kolibrify/predict.py
@@ -4,6 +4,7 @@ from peft import PeftModel
 import os
 import vllm
 import json
+from copy import deepcopy
 
 from kolibrify.config import TrainingConfig, load_training_config
 from kolibrify.model_utils import get_model
@@ -12,7 +13,7 @@ from kolibrify.data_utils import format_chatml
 
 class VllmModel:
     def __init__(self, merged_model_path: str, config: TrainingConfig, temp=0.0, top_p=0.95, max_tokens=4096):
-        self.vllm_model = vllm.LLM(merged_model_path, max_model_len=config.max_ctx_len)
+        self.vllm_model = vllm.LLM(merged_model_path, max_model_len=max_tokens)
         self.sampling_params = vllm.SamplingParams(temperature=temp, top_p=top_p, max_tokens=max_tokens)
 
     def predict(self, convs):
@@ -61,17 +62,15 @@ def format_prompts_vllm(messages):
 
 
 def predict(
-    config_path: Annotated[str, typer.Argument()],
-    dataset_path: Annotated[str, typer.Argument()],
-    dataset_save_path: Annotated[str, typer.Argument()] = 'output.jsonl',
-    backend: Annotated[str, typer.Option()] = 'vllm',
-    type: Annotated[str, typer.Option()] = 'last',
-    temp: Annotated[float, typer.Option()] = 0,
-    top_p: Annotated[float, typer.Option()] = 0.95,
-    max_output_tokens: Annotated[int, typer.Option()] = 4096
+    config: TrainingConfig,
+    conversations: list,
+    backend: str = 'vllm',
+    type: str = 'last',
+    temp: float = 0,
+    top_p: float = 0.95,
+    max_output_tokens: int = 4096
 ):
-    _, config = load_training_config(config_path)
-    conversations = load_dataset(dataset_path)
+    conversations = deepcopy(conversations)
     
     model_path = os.path.join(config.output_dir, 'merged')
     if backend == 'vllm':
@@ -86,9 +85,34 @@ def predict(
     for conv, response in zip(conversations, responses):
         conv['messages'].append(response)
         
+    return conversations
+
+
+def main(
+    config_path: Annotated[str, typer.Argument()],
+    dataset_path: Annotated[str, typer.Argument()],
+    dataset_save_path: Annotated[str, typer.Argument()] = 'output.jsonl',
+    backend: Annotated[str, typer.Option()] = 'vllm',
+    type: Annotated[str, typer.Option()] = 'last',
+    temp: Annotated[float, typer.Option()] = 0,
+    top_p: Annotated[float, typer.Option()] = 0.95,
+    max_output_tokens: Annotated[int, typer.Option()] = 4096
+):
+    _, config = load_training_config(config_path)
+    conversations = load_dataset(dataset_path)
+    
+    conversations = predict(
+        config=config,
+        conversations=conversations,
+        backend=backend,
+        type=type,
+        temp=temp,
+        top_p=top_p,
+        max_output_tokens=max_output_tokens
+    )
+    
     save_dataset(conversations, dataset_save_path)
     print('Saved responses to', dataset_save_path)
 
-
 def run():
-    typer.run(predict)
+    typer.run(main)
