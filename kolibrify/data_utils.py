d1b22f5dd2a4468de5a3943aaeaa000ca091fdab
oKatanaaa <whitemarsstudios@gmail.com>
2024-05-21 22:44:56 +0000

    fix(data_utils): curriculum sampling

--- kolibrify/data_utils.py
+++ kolibrify/data_utils.py
@@ -1,52 +1,17 @@
 import datasets
+datasets.disable_caching()
 import random
 import copy
 import json
 from typing import List
+from tqdm import tqdm
 
 from .sft.config import StageConfig, DatasetConfig
-
-class CurriculumDataGen:
-    def __init__(self, simple_data_gens):
-        self.datagens = simple_data_gens
-        
-    def __iter__(self):
-        for stage_name, datagen in self.datagens.items():
-            print(f'Stage {stage_name} data sampling.')
-            for sample in datagen:
-                yield sample
-
-    def __call__(self):
-        return self.__iter__()
+from .core.config import BaseConfig
+from .core.data_utils import SimpleDataGen, CurriculumDataGen, ChatMLFormatter
 
 
-class SimpleDataGen:
-    def __init__(self, samples, epochs: float):
-        self.samples = copy.deepcopy(samples)
-        random.shuffle(self.samples)
-        self.iterations = int(len(self.samples) * epochs)
-        self.current_iter = -1
-        
-    def __iter__(self):
-        return self
-            
-    def __next__(self):
-        self.current_iter += 1
-        if self.current_iter == self.iterations:
-            raise StopIteration()
-        
-        if self.current_iter % len(self.samples) == 0:
-            # An epoch has passed, reshuffle dataset
-            random.shuffle(self.samples)
-        
-        idx = self.current_iter % len(self.samples)
-        return self.samples[idx]
-
-    def __call__(self):
-        return self.__iter__()
-
-
-def load_jsonl_data(datasets: List[DatasetConfig], epochs: float):
+def load_jsonl_data(datasets: List[DatasetConfig], epochs: float, format_fn_batched=None):
     """
     Loads a dataset which samples stored as jsons per line.
     
@@ -73,45 +38,39 @@ def load_jsonl_data(datasets: List[DatasetConfig], epochs: float):
         print(f'Read {len(samples)} samples from {dataset.path}.')
         total_samples.extend(samples)
     print(f'Total samples accumulated: {len(total_samples)}')
+
+    if format_fn_batched is not None:
+        print('format_fn is not None, applying format_fn')
+        total_samples = format_fn_batched(total_samples)
+        # _total_samples = []
+        # for sample in tqdm(total_samples):
+        #     _total_samples.append(format_fn(sample))
+        # total_samples = _total_samples
+        print('format_fn applied')
+
     return SimpleDataGen(total_samples, epochs)
 
 
-def load_dataset(stages: List[StageConfig], val_dataset_path=None, format_fn=None):
-    if format_fn is None:
-        format_fn = format_chatml
+def load_dataset(stages: List[StageConfig], tokenizer, config: BaseConfig, val_dataset_path=None):
+    format_fn  = ChatMLFormatter(tokenizer, config.max_ctx_len)
 
     training_datagens = {}
     total_data_iterations = 0
     for stage in stages:
         epochs = stage.epochs
         dataset_configs = stage.datasets
-        datagen = load_jsonl_data(dataset_configs, epochs)
+        datagen = load_jsonl_data(dataset_configs, epochs, format_fn_batched=None)
         total_data_iterations += datagen.iterations
         training_datagens[stage.name] = datagen
     
     curriculum_datagen = CurriculumDataGen(training_datagens)
 
-    train_dataset = datasets.Dataset.from_generator(curriculum_datagen) \
-        .map(format_chatml, load_from_cache_file=False)
+    train_dataset = datasets.IterableDataset.from_generator(curriculum_datagen) \
+        .map(format_fn.format_batched, batch_size=config.micro_batch_size, batched=True)
     
     val_dataset = None
     if val_dataset_path:
         validation_gen = load_jsonl_data(val_dataset_path)
         val_dataset = datasets.Dataset.from_generator(validation_gen) \
-            .map(format_chatml, load_from_cache_file=False)
+            .map(format_fn, load_from_cache_file=False)
     return train_dataset, val_dataset, total_data_iterations
-
-
-def format_chatml(chat: list[dict[str, str]] = dict()) -> str:
-    """
-    Uses https://github.com/openai/openai-python/blob/main/chatml.md as chat format.
-    """
-    chat = chat['messages']
-    raw_chat_text = ""
-    for item in chat:
-        if len(raw_chat_text) > 0:
-            raw_chat_text += '\n'
-        role = item['role']
-        content = item['content']
-        raw_chat_text += f"<|im_start|>{role}\n{content}<|im_end|>"
-    return {'prompt': raw_chat_text}
